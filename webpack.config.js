beed82fdca40562400acb00c0c31225bbe7958af
JSPENKAR <jspenkar@gmail.com>
2017-01-28 15:16:44 -0600

    Packages added.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: './dist/bundle.js'
+  },
+  plugins: [
+    new webpack.OldWatchingPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jpg$/,
+        loader: 'file-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+        exclude: /node_modules/
+      },
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'stage-0', 'react']
+        }
+      }
+    ]
+  },
+  watch: true
+};
