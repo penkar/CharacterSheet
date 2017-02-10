b7d42c63e1d8847692b05f70053c36c257327f48
= <jspenkar@gmail.com>
2017-02-09 20:27:52 -0600

    Header2 flexboxxed.

--- src/js/components/dnd/Header2.js
+++ src/js/components/dnd/Header2.js
@@ -6,74 +6,72 @@ export default class Header2 extends Component {
   _row1(change) {
     let {props} = this;
     return (
-      <tr>
-        <td></td>
-        <td className='ll t'>
+      <div className='header-row'>
+        <div></div>
+        <div className='ll t'>
           <Input placeholder={props.age} value={props.age} specific='' field='age' change={change}/>
-        </td>
-        <td className='t'>
+        </div>
+        <div className='t'>
           <Input placeholder={props.height} value={props.height} specific='' field='height' change={change}/>
-        </td>
-        <td className='rr t'>
+        </div>
+        <div className='rr t'>
           <Input placeholder={props.weight} value={props.weight} specific='' field='weight' change={change}/>
-        </td>
-      </tr>
+        </div>
+      </div>
     )
   }
 
   _row2() {
     return (
-      <tr>
-        <td></td>
-        <td className='ll'>Age</td>
-        <td>Height</td>
-        <td className='rr'>Weight</td>
-      </tr>
+      <div className='header-row'>
+        <div></div>
+        <div className='ll'>Age</div>
+        <div>Height</div>
+        <div className='rr'>Weight</div>
+      </div>
     )
   }
 
   _row3(change) {
     let {props} = this;
     return (
-      <tr>
-        <td className='ll t b'>
+      <div className='header-row'>
+        <div className='ll t b'>
           <Input placeholder={props.characterName} value={props.characterName} specific='' field='characterName' change={change}/>
-        </td>
-        <td className='ll'>
+        </div>
+        <div>
           <Input placeholder={props.eyes} value={props.eyes} specific='' field='eyes' change={change}/>
-        </td>
-        <td>
+        </div>
+        <div>
           <Input placeholder={props.skin} value={props.skin} specific='' field='skin' change={change}/>
-        </td>
-        <td className='rr'>
+        </div>
+        <div className='rr'>
           <Input placeholder={props.hair} value={props.hair} specific='' field='hair' change={change}/>
-        </td>
-      </tr>
+        </div>
+      </div>
     )
   }
 
   _row4() {
     return (
-      <tr>
-        <td>Character Name</td>
-        <td className='ll b'>Eyes</td>
-        <td className='b'>Skin</td>
-        <td className='rr b'>Hair</td>
-      </tr>
+      <div className='header-row'>
+        <div>Character Name</div>
+        <div className='ll b'>Eyes</div>
+        <div className='b'>Skin</div>
+        <div className='rr b'>Hair</div>
+      </div>
     )
   }
 
   render() {
     let {change} = this.props;
     return (
-      <table id='sheet-header' cellSpacing='0'>
-        <tbody>
-          {::this._row1(change)}
-          {::this._row2()}
-          {::this._row3(change)}
-          {::this._row4()}
-        </tbody>
-      </table>
+      <div id='header2'>
+        {::this._row1(change)}
+        {::this._row2()}
+        {::this._row3(change)}
+        {::this._row4()}
+      </div>
     )
   }
 }
