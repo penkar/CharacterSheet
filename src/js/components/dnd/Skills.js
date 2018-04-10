f952e60f85fb64640ffcf4736da151489010066b
JSP <jspenkar@gmail.com>
2018-04-10 18:48:59 -0500

    Additional function change.

--- src/js/components/dnd/Skills.js
+++ src/js/components/dnd/Skills.js
@@ -1,44 +1,10 @@
 import React from 'react'
 import {Inspiration, Proficiency, SkillScores, SavingThrows,} from './skill/'
-
-export function Skills ({
-    acrobatics, 
-    animalHandling,
-    arcana,
-    athletics,
-    deception,
-    history,
-    insight,
-    intimidation,
-    investigation,
-    medicine,
-    nature,
-    perception,
-    performance,
-    persuation,
-    religion,
-    sleightOfHand,
-    stealth,
-    survival,
-    inspiration,
-    proficiencyBonus,
-    strengthSaving,
-    dexteritySaving,
-    constitutionSaving,
-    intelligenceSaving,
-    wisdomSaving,
-    charismaSaving
-  }, change, abi) {
-    
-  let saving = {strengthSaving, dexteritySaving, constitutionSaving, intelligenceSaving, wisdomSaving, charismaSaving,}
-  let skillset = {acrobatics, animalHandling, arcana, athletics, deception, history, insight, intimidation, investigation, medicine, nature, perception, performance, persuation, religion, sleightOfHand, stealth, survival};
-  let pro = proficiencyBonus.value || proficiencyBonus.placeholder;
-  return (
-    <div id='skills' className='sectional'>
-      { Inspiration(inspiration, change) }
-      { Proficiency(proficiencyBonus, change) }
-      { SavingThrows(saving, change, pro, abi) }
-      { SkillScores(skillset, change, pro, abi) }
-    </div>
-  )
-}
+export const Skills = (skills, change, abi) => (
+  <div id='skills' className='sectional'>
+    { Inspiration(skills.inspiration, change) }
+    { Proficiency(skills.proficiencyBonus, change) }
+    { SavingThrows(skills, change, skills.proficiencyBonus.value || skills.proficiencyBonus.placeholder, abi) }
+    { SkillScores(skills, change, skills.proficiencyBonus.value || skills.proficiencyBonus.placeholder, abi) }
+  </div>
+);
