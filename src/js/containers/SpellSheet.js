edb9008e7f7ae28bad593ab2d468ab93c302cc0b
JSP <jspenkar@gmail.com>
2018-04-26 21:00:17 -0500

    Components added to spells.

--- src/js/containers/SpellSheet.js
+++ src/js/containers/SpellSheet.js
@@ -1,5 +1,24 @@
-import React from 'react'
-import {Spells} from '../components/dnd/magic/Spells1.js'
+import React from 'react';
+import cn from 'classnames';
+import {Spells} from '../components/dnd/magic/Spells1.js';
+
+class Spell extends React.Component {
+  shouldComponentUpdate = (nextProps) => nextProps.hide !==  this.props.hide;
+  render() {
+    let {name, lvl, cst, desc, dur, rng, type, hide} = this.props;
+    return (
+      <div key={name} className={cn('pure-u-1', 'pure-u-lg-24-24', 'sectional', {hide})}>
+        <div key='name'>Name: {name}</div>
+        <div key='lvl'>Level: {lvl}</div>
+        <div key='cst'>Casting: {cst}</div>
+        <div key='desc'>Description: {desc}</div>
+        <div key='dur'>Duration: {dur}</div>
+        <div key='r'>Range: {rng}</div>
+        <div key='s'>Type: {type}</div>
+      </div>
+    );
+  }
+}
 
 const SpellSheet = ({filter, change}) => {
   let _onChange = ({target}) => {
@@ -10,19 +29,7 @@ const SpellSheet = ({filter, change}) => {
       <div key='input' className='pure-u-1 pure-u-lg-24-24 sectional'>
         <input onChange={_onChange} name='spell-filter' placeholder="Filter spells by..." />
       </div>
-      { [...Spells]
-        .filter( ({name}) => !!name.match(regex) )
-        .map( ({name, lvl, cst, desc, dur, rng, type}) =>
-        <div key={name} className='pure-u-1 pure-u-lg-24-24 sectional'>
-          <div key='name'>Name: {name}</div>
-          <div key='lvl'>Level: {lvl}</div>
-          <div key='cst'>Casting: {cst}</div>
-          <div key='desc'>Description: {desc}</div>
-          <div key='dur'>Duration: {dur}</div>
-          <div key='r'>Range: {rng}</div>
-          <div key='s'>Type: {type}</div>
-        </div>
-      ) }
+      { Spells.map((spell) => <Spell {...spell} hide={!spell.name.match(regex)} /> ) }
     </div>
   )
 }
