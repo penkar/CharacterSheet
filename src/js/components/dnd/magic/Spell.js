823e84e659d76325de89f9860a927f31a8639b36
Jeffrey Penkar <jspenkar@gmail.com>
2018-07-22 11:00:55 -0500

    Ability to go straight from spell to its description added.

--- src/js/components/dnd/magic/Spell.js
+++ src/js/components/dnd/magic/Spell.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {CBox} from '../../generics/CBox';
-export const Spell = ({i, change, level, name, prepared}) =>
+export const Spell = ({i, change, level, name, prepared, changeSettings}) =>
   <div className='spell' key={name}>
     { CBox({click: function(){ change({level, i, prepared:!prepared})}, checked:prepared}) }
-    <div className='name'>{name}</div>
+    <div className='name' onClick={()=>{changeSettings({filter:name, view:'magic'})}}>{name}</div>
     <span className='minus' onClick={() => change({level, i, oldspell:true})} />
   </div>
